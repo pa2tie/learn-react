1e11c73258abe78be6b516189ef60ce3f9f79560
pa2tie <myadsagain@gmail.com>
2018-12-19 19:08:13 +0300

    intermediate

--- src/app.jsx
+++ src/app.jsx
@@ -1,67 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MercuryLogo from "./components/MercuryLogo";
-import MercuryLogin from "./components/MercuryLogin";
-import MercuryProfile from "./components/MercuryProfile";
-import { Provider } from "./store/Context";
-import styles from "./css/style.css";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      user: null
-    };
-
-    this.props.history.push("/login");
-  }
-
-  handleSubmit = user => {
-    this.setState({
-      user
-    });
-    this.props.history.push("/profile");
-  };
-
-  logout = () => {
-    this.setState({
-      user: null
-    });
-    this.props.history.push("/login");
-  };
-
-  render() {
-    return (
-      <section className={styles["mercury"]}>
-        <MercuryLogo href="#/login" />
-        <Switch>
-          <Provider value={this.state.user}>
-            {this.state.user && (
-              <Route
-                exact
-                path="/profile"
-                render={props => <MercuryProfile onLogout={this.logout} />}
-              />
-            )}
-            <Route
-              exact
-              path="/login"
-              render={props => (
-                <MercuryLogin handleSubmit={this.handleSubmit} />
-              )}
-            />
-          </Provider>
-        </Switch>
-      </section>
-    );
-  }
-}
+import MercuryHomeScreen from "./components/MercuryHomeScreen/MercuryHomeScreen";
 
 ReactDOM.render(
   <Router>
-    <Route path="/" component={App} />
+    <Route path="/" component={MercuryHomeScreen} />
   </Router>,
   document.getElementById("root")
 );
--- src/components/MercuryHomeScreen/MercuryHomeScreen.jsx
+++ src/components/MercuryHomeScreen/MercuryHomeScreen.jsx
@@ -1,1 +1,62 @@
-// HomeScreen
+import React from "react";
+import ReactDOM from "react-dom";
+import MercuryLogo from "./MercuryLogo/MercuryLogo";
+import MercuryLogin from "./MercuryLogin/MercuryLogin";
+import MercuryProfile from "./MercuryProfile/MercuryProfile";
+import { Provider } from "../../store/Context";
+import styles from "./MercuryHomeScreen.css";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+class MercuryHomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null
+    };
+
+    this.props.history.push("/login");
+  }
+
+  handleSubmit = user => {
+    this.setState({
+      user
+    });
+    this.props.history.push("/profile");
+  };
+
+  logout = () => {
+    this.setState({
+      user: null
+    });
+    this.props.history.push("/login");
+  };
+
+  render() {
+    return (
+      <section className={styles["mercury"]}>
+        <MercuryLogo href="#/login" />
+        <Switch>
+          <Provider value={this.state.user}>
+            {this.state.user && (
+              <Route
+                exact
+                path="/profile"
+                render={props => <MercuryProfile onLogout={this.logout} />}
+              />
+            )}
+            <Route
+              exact
+              path="/login"
+              render={props => (
+                <MercuryLogin handleSubmit={this.handleSubmit} />
+              )}
+            />
+          </Provider>
+        </Switch>
+      </section>
+    );
+  }
+}
+
+export default MercuryHomeScreen;
